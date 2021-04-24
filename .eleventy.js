65475a8848241afa4ab3878a4bb793fdad381798
Mark Evans <mark@hellobaton.com>
2021-04-23 22:23:02 -0400

    ayyyy

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,30 @@
-module.exports = {
-  dir: {
-    input: 'src',
-    output: '_site',
-  }
+const { format } = require('date-fns');
+
+module.exports = (eleventyConfig) => {
+  eleventyConfig.addPassthroughCopy('img');
+  eleventyConfig.addPassthroughCopy('css');
+
+  eleventyConfig.addFilter('readableDate', date => {
+    return format(date, 'EEEE MMMM d, yyyy');
+  });
+
+  eleventyConfig.setFrontMatterParsingOptions({
+    excerpt: true,
+    excerpt_separator: "<!-- excerpt -->",
+  })
+
+  eleventyConfig.setTemplateFormats([
+    'md',
+    'njk',
+    'css',
+    'jpg',
+  ]);
+
+  return {
+    passthroughFileCopy: true,
+    dir: {
+      input: 'src',
+      layouts: '_layouts',
+    },
+  };
 }
